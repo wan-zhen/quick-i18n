5499ba862bf521a50470642b50dd9c6dc8631a27
zhen <kelly811124811124@hotmail.com>
2020-02-12 13:31:13 +0800

    over write i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,7 +5,29 @@ import { Schema } from './schema';
 // per file.
 export function i18n(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log(_options.key)
+    let [enText, cnText, viText] = [
+      JSON.parse((tree.read('./src/assets/i18n/en-US.json')?.toString('utf-8') as string)),
+      JSON.parse((tree.read('./src/assets/i18n/zh-CN.json')?.toString('utf-8') as string)),
+      JSON.parse((tree.read('./src/assets/i18n/vi-VN.json')?.toString('utf-8') as string))
+    ];
+    let [type, key] = _options.key.split('.');
+    let { cn, en, vi } = _options;
+    if (!enText[type]) {
+      enText[type] = {};
+    }
+    if (!cnText[type]) {
+      cnText[type] = {};
+    }
+    if (!viText[type]) {
+      viText[type] = {};
+    }
+    enText[type][key] = en;
+    cnText[type][key] = cn;
+    viText[type][key] = vi;
+    tree.overwrite('./src/assets/i18n/en-US.json', JSON.stringify(enText, null, "\t"));
+    tree.overwrite('./src/assets/i18n/zh-CN.json', JSON.stringify(cnText, null, "\t"));
+    tree.overwrite('./src/assets/i18n/vi-VN.json', JSON.stringify(viText, null, "\t"));
+
     return tree;
   };
 }
